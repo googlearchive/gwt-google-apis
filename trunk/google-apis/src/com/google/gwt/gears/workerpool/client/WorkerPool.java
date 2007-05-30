32ecf4c999a30c46b62d16b3f8dec90134e97dd4
gwt.team.mmendez <gwt.team.mmendez@dc8c9c99-1131-0410-92e2-872299cb2494>
2007-05-30 23:04:09 +0000

    Initial import

---
+++ trunk/google-apis/src/com/google/gwt/gears/workerpool/client/WorkerPool.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2007 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.gears.workerpool.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.gears.core.client.Gears;
+import com.google.gwt.gears.core.client.GearsException;
+import com.google.gwt.gears.core.client.impl.GearsImpl;
+
+/**
+ * Used to create and manage JavaScript threads using the Gears WorkerPool
+ * system. A WorkerPool represents a group of JavaScript threads. Each thread
+ * runs in a separate JavaScript VM, and has an isolated namespace. That is,
+ * threads do not have access to any other threads' variables, including their
+ * parents'.
+ * 
+ * WorkerPools can be used to group threads for convenience.
+ * 
+ * Currently this class can only create worker threads out of raw JavaScript
+ * code. That is, user code cannot currently create worker bodies from Java
+ * code.
+ */
+public class WorkerPool {
+  /*
+   * This method is called from JSNI.
+   */
+  private static void fireOnMessageReceived(WorkerPool pool, String msg,
+      int srcWorkerId) {
+    if (pool.callback != null) {
+      pool.callback.onMessageReceived(msg, srcWorkerId);
+    }
+  }
+
+  /**
+   * Native method that calls <code>createWorker</code> on the underlying
+   * <code>pool</code> object. Note that this method can throw an unchecked
+   * JavaScriptException.
+   * 
+   * @param javaScript the JavaScript code to pass into the worker thread
+   * @return the ID of the newly-created thread
+   */
+  private static native int nativeCreateWorkerByString(JavaScriptObject pool,
+      String javaScript) throws GearsException /*-{
+   try {
+   return pool.createWorker(javaScript);
+   } catch (e) {
+   @com.google.gwt.gears.workerpool.client.WorkerPool::throwGearsException(Ljava/lang/String;)(e.toString());
+   }
+   }-*/;
+
+  private static native void nativeSendMessage(JavaScriptObject pool,
+      String message, int destWorker) throws GearsException /*-{
+   try {
+   pool.sendMessage(message, destWorker);
+   } catch (e) {
+   @com.google.gwt.gears.workerpool.client.WorkerPool::throwGearsException(Ljava/lang/String;)(e.toString());
+   }
+   }-*/;
+
+  /**
+   * Sets the raw JavaScript onmessage handler to a function which dynamically
+   * dispatches to the current callback object, if one is registered.
+   * 
+   */
+  private static native void setOnMessage(WorkerPool workerPool,
+      JavaScriptObject jsPool) /*-{
+   var jPool = workerPool;
+   jsPool.onmessage = function(msg, srcId) {
+   @com.google.gwt.gears.workerpool.client.WorkerPool::fireOnMessageReceived(Lcom/google/gwt/gears/workerpool/client/WorkerPool;Ljava/lang/String;I)(jPool, msg, srcId);
+   }
+   }-*/;
+
+  private static void throwGearsException(String message) throws GearsException {
+    throw new GearsException(message);
+  }
+
+  /**
+   * The callback function registered to this worker thread.
+   */
+  private final MessageHandler callback;
+
+  /**
+   * Handle to the native WorkerPool object wrapped by this class.
+   */
+  private final JavaScriptObject pool;
+
+  /**
+   * Constructs a WorkerPool object.
+   * 
+   * @param callback if non-<code>null</code>, the instance which will
+   *          receive messages from threads
+   * 
+   * @throws GearsException if the WorkerPool could not be constructed
+   */
+  public WorkerPool(MessageHandler callback) throws GearsException {
+    this(callback, Gears.WORKERPOOL, Gears.GEARS_VERSION);
+  }
+
+  /**
+   * Constructs a WorkerPool object backed by the provided Gears pool object.
+   * 
+   * @throws GearsException if the WorkerPool could not be constructed
+   */
+  protected WorkerPool(MessageHandler callback, String className,
+      String classVersion) throws GearsException {
+    this.callback = callback;
+    pool = GearsImpl.create(className, classVersion);
+    setOnMessage(this, pool);
+  }
+
+  /**
+   * Creates a JavaScript thread using the JavaScript code stored in
+   * <code>javaScript</code>. The code in <code>javaScript</code> is
+   * guaranteed to complete before this method returns. The code must set an
+   * <code>onmessage</code> handler (though it may be a no-op handler.)
+   * 
+   * @param javaScript the JavaScript code to run in the thread
+   * @return the ID of the newly-created thread
+   * @throws GearsException if the worker could not be created
+   * @throws NullPointerException if the java script string is <code>null</code>
+   */
+  public int createWorkerFromString(String javaScript) throws GearsException {
+    if (javaScript == null) {
+      throw new NullPointerException();
+    }
+
+    return nativeCreateWorkerByString(pool, javaScript);
+  }
+
+  /**
+   * Sends a message with the indicated data to the indicated thread. Messages
+   * can only be sent between members of the same <code>WorkerPool</code>.
+   * 
+   * @param message the data to send
+   * @param destWorker the thread to send the data to
+   * @throws NullPointerException of <code>message</code> is <code>null</code>
+   * @throws GearsException if the <code>message</code> cannot be sent to the 
+   * specified <code>destWorker</code>
+   */
+  public void sendMessage(String message, int destWorker) throws GearsException {
+    if (message == null) {
+      throw new NullPointerException();
+    }
+    nativeSendMessage(pool, message, destWorker);
+  }
+  
+  /**
+   * Returns the JavaScript <code>WorkerPool</code> object wrapped by this
+   * instance.
+   * 
+   * @return the JavaScript <code>WorkerPool</code> object wrapped by this
+   *         instance
+   */
+  protected final JavaScriptObject getJavaScriptObject() {
+    return pool;
+  }
+}
---
+++ trunk/google-apis/test/com/google/gwt/gears/localserver/client/LocalServerTest.java
@@ -0,0 +1,282 @@
+/*
+ * Copyright 2007 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.gears.localserver.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.gears.core.client.Gears;
+import com.google.gwt.gears.core.client.GearsException;
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * 
+ */
+public class LocalServerTest extends GWTTestCase {
+  private static final String RESOURCE_STORE_NAME = "TestResourceStore";
+  private static final String MANAGED_RESOURCE_STORE_NAME = "TestManagedResourceStore";
+
+  private static String getMyURL() {
+    return GWT.getModuleBaseURL() + "HelloWorld.js";
+  }
+
+  public String getModuleName() {
+    return "com.google.gwt.gears.localserver.LocalServerTest";
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#canServeLocally(java.lang.String) canServeLocally("")}.
+   * 
+   * @throws GearsException
+   */
+  public void testCanServeLocallyEmptyURL() throws GearsException {
+    LocalServer ls = new LocalServer();
+    try {
+      ls.canServeLocally("");
+    } catch (IllegalArgumentException e) {
+      // Expected to get here
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#canServeLocally(java.lang.String) canServeLocally(null)}.
+   * 
+   * @throws GearsException
+   */
+  public void testCanServeLocallyNullURL() throws GearsException {
+    LocalServer ls = new LocalServer();
+    try {
+      ls.canServeLocally(null);
+      fail("Expected NullPointerException");
+    } catch (NullPointerException e) {
+      // Expected to get here
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#canServeLocally(java.lang.String)}.
+   * 
+   * @throws GearsException
+   */
+  public void testCanServeLocallySameOriginURLCaptured() throws GearsException {
+    final LocalServer ls = new LocalServer();
+    delayTestFinish(5000);
+    String requestedURL = getMyURL();
+    final ResourceStore rs = ls.createResourceStore(RESOURCE_STORE_NAME);
+    rs.removeURL(requestedURL);
+    assertFalse(ls.canServeLocally(requestedURL));
+    rs.captureURL(requestedURL, new URLCaptureCallback() {
+      public void onCaptureFailure(String url, int captureId) {
+        fail("Could not capture: " + url);
+      }
+
+      public void onCaptureSuccess(String url, int captureId) {
+        boolean failed = true;
+        try {
+          failed = !ls.canServeLocally(url);
+        } catch (GearsException e) {
+          // we failed, ignore the exception
+        }
+        assertFalse(failed);
+        finishTest();
+      }
+    });
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#canServeLocally(java.lang.String))}.
+   * 
+   * @throws GearsException
+   */
+  public void testCanServeLocallySameOriginURLNotCaptured()
+      throws GearsException {
+    LocalServer ls = new LocalServer();
+    assertFalse(ls.canServeLocally(GWT.getModuleBaseURL() + "DoesNotExist.html"));
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#createManagedResourceStore(java.lang.String)}.
+   * 
+   * @throws GearsException
+   */
+  public void testCreateManagedStoreString() throws GearsException {
+    LocalServer ls = new LocalServer();
+    ManagedResourceStore mrs = ls.createManagedResourceStore(MANAGED_RESOURCE_STORE_NAME);
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#createManagedResourceStore(java.lang.String, java.lang.String)}.
+   * 
+   * @throws GearsException
+   */
+  public void testCreateManagedStoreStringString() throws GearsException {
+    LocalServer ls = new LocalServer();
+    ManagedResourceStore mrs = ls.createManagedResourceStore(
+        MANAGED_RESOURCE_STORE_NAME, "foo");
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#createResourceStore(java.lang.String)}.
+   * 
+   * @throws GearsException
+   */
+  public void testCreateResourceStoreString() throws GearsException {
+    LocalServer ls = new LocalServer();
+    ResourceStore rs = ls.createResourceStore(RESOURCE_STORE_NAME);
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#createResourceStore(java.lang.String, java.lang.String)}.
+   * 
+   * @throws GearsException
+   */
+  public void testCreateResourceStoreStringString() throws GearsException {
+    LocalServer ls = new LocalServer();
+    ResourceStore rs = ls.createResourceStore(RESOURCE_STORE_NAME, "bar");
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#LocalServer(java.lang.String, java.lang.String)}.
+   * 
+   * @throws GearsException
+   */
+  public void testLocalServerStringString() throws GearsException {
+    LocalServer ls = new LocalServer(Gears.LOCALSERVER, Gears.GEARS_VERSION);
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#openManagedResourceStore(java.lang.String)}.
+   * 
+   * @throws GearsException
+   */
+  public void testOpenManagedResourceStoreString() throws GearsException {
+    LocalServer ls = new LocalServer();
+    try {
+      assertNull(ls.openManagedResourceStore("DOES_NOT_EXIST"));
+
+      ls.createManagedResourceStore(MANAGED_RESOURCE_STORE_NAME);
+      assertNotNull(ls.openManagedResourceStore(MANAGED_RESOURCE_STORE_NAME));
+    } finally {
+      ls.removeManagedResourceStore(MANAGED_RESOURCE_STORE_NAME);
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#openManagedResourceStore(java.lang.String, java.lang.String)}.
+   * 
+   * @throws GearsException
+   */
+  public void testOpenManagedResourceStoreStringString() throws GearsException {
+    LocalServer ls = new LocalServer();
+    try {
+      assertNull(ls.openManagedResourceStore("DOES_NOT_EXIST", "foo"));
+      ls.createManagedResourceStore(MANAGED_RESOURCE_STORE_NAME, "foo");
+      assertNotNull(ls.openManagedResourceStore(MANAGED_RESOURCE_STORE_NAME,
+          "foo"));
+    } finally {
+      ls.removeManagedResourceStore(MANAGED_RESOURCE_STORE_NAME, "foo");
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#openResourceStore(java.lang.String)}.
+   * 
+   * @throws GearsException
+   */
+  public void testOpenResourceStoreString() throws GearsException {
+    LocalServer ls = new LocalServer();
+
+    try {
+      assertNull(ls.openResourceStore("DOES_NOT_EXIST"));
+      ls.createResourceStore(RESOURCE_STORE_NAME);
+      assertNotNull(ls.openResourceStore(RESOURCE_STORE_NAME));
+    } finally {
+      ls.removeResourceStore(RESOURCE_STORE_NAME);
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#openStore(java.lang.String, java.lang.String)}.
+   * 
+   * @throws GearsException
+   */
+  public void testOpenResourceStoreStringString() throws GearsException {
+    LocalServer ls = new LocalServer();
+    try {
+      assertNull(ls.openResourceStore("DOES_NOT_EXIST", "foo"));
+      ls.createResourceStore(RESOURCE_STORE_NAME, "foo");
+      ls.openResourceStore(RESOURCE_STORE_NAME, "foo");
+    } finally {
+      ls.removeResourceStore(RESOURCE_STORE_NAME, "foo");
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#removeManagedResourceStore(java.lang.String)}.
+   * 
+   * @throws GearsException
+   */
+  public void testRemoveManagedResourceStoreString() throws GearsException {
+    LocalServer ls = new LocalServer();
+    ls.createManagedResourceStore(MANAGED_RESOURCE_STORE_NAME);
+    ls.removeManagedResourceStore(MANAGED_RESOURCE_STORE_NAME);
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#removeManagedResourceStore(java.lang.String, java.lang.String)}.
+   * @throws GearsException 
+   */
+  public void testRemoveManagedResourceStoreStringString() throws GearsException {
+    LocalServer ls = new LocalServer();
+    ls.createManagedResourceStore(MANAGED_RESOURCE_STORE_NAME, "foo");
+    ls.removeManagedResourceStore(MANAGED_RESOURCE_STORE_NAME, "foo");    
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#removeResourceStore(java.lang.String)}.
+   * @throws GearsException 
+   */
+  public void testRemoveStoreString() throws GearsException {
+    LocalServer ls = new LocalServer();
+    ls.createResourceStore(RESOURCE_STORE_NAME);
+    ls.removeResourceStore(RESOURCE_STORE_NAME);    
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.gwt.gears.localserver.client.LocalServer#removeResourceStore(java.lang.String, java.lang.String)}.
+   * @throws GearsException 
+   */
+  public void testRemoveStoreStringString() throws GearsException {
+    LocalServer ls = new LocalServer();
+    ls.createResourceStore(RESOURCE_STORE_NAME, "foo");
+    ls.removeResourceStore(RESOURCE_STORE_NAME, "foo");        
+  }
+}
