45785314cb9b09eec427886571a6d980bf10e02b
gwt.team.mmendez <gwt.team.mmendez@dc8c9c99-1131-0410-92e2-872299cb2494>
2007-05-31 06:22:24 +0000

    Add GWT_HOME checks to the build scripts.

--- samples/database/launch-scripts/windows/DatabaseDemo-shell.cmd
+++ samples/database/launch-scripts/windows/DatabaseDemo-shell.cmd
@@ -1,3 +1,7 @@
-@java -cp "%~dp0\..\src;%~dp0\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\gwt-google-apis.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\www" %* com.google.gwt.gears.sample.database.DatabaseDemo/DatabaseDemo.html
+@if "%GWT_HOME%"=="" goto needGWTHome
+@java -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-google-apis.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\..\www" %* com.google.gwt.gears.sample.database.DatabaseDemo/DatabaseDemo.html
+@exit /B %ERRORLEVEL%
 
-
+:needGWTHome
+@echo The environment variable GWT_HOME is not defined, it should point to a valid GWT installation.
+@exit /B 1
--- samples/resourcestore/launch-scripts/windows/ResourceStoreDemo-compile.cmd
+++ samples/resourcestore/launch-scripts/windows/ResourceStoreDemo-compile.cmd
@@ -1,1 +1,7 @@
-@java -cp "%~dp0\..\src;%~dp0\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\gwt-google-apis.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\..\www" %* com.google.gwt.gears.sample.resourcestore.ResourceStoreDemo
+@if "%GWT_HOME%"=="" goto needGWTHome
+@java -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-google-apis.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\..\..\www" %* com.google.gwt.gears.sample.resourcestore.ResourceStoreDemo
+@exit /B %ERRORLEVEL%
+
+:needGWTHome
+@echo The environment variable GWT_HOME is not defined, it should point to a valid GWT installation.
+@exit /B 1
