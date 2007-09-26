df7d084dc221cb5b22751688d9dc4dd08886f1f6
gwt.team.mmendez <gwt.team.mmendez@dc8c9c99-1131-0410-92e2-872299cb2494>
2007-09-26 17:50:31 +0000

    Fixed some places where we failed to refactor package names.
    Patch by: mmendez, sumitchandel (pair prog)
    Review by: sumitchandel, mmendez

--- trunk/samples/maps/launch-scripts/windows/MapsSample-shell.cmd
+++ trunk/samples/maps/launch-scripts/windows/MapsSample-shell.cmd
@@ -1,5 +1,5 @@
 @if "%GWT_HOME%"=="" goto needGWTHome
-@java -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-google-apis.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\..\www" %* com.google.gwt.maps.sample.maps.MapsSample/MapsSample.html
+@java -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-google-apis.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\..\www" %* com.google.gwt.maps.sample.maps.HelloMaps/HelloMaps.html
 @exit /B %ERRORLEVEL%
 
 :needGWTHome
