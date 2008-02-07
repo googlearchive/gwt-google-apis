b9b52eb5ed4f3c55d5f05edb00d00bd4acbc9bcb
gwt.team.mmendez <gwt.team.mmendez@dc8c9c99-1131-0410-92e2-872299cb2494>
2008-02-07 16:59:45 +0000

    Sort, format, and remove redundant modifiers on IconImpl.

--- google-apis/src/com/google/gwt/maps/client/impl/IconImpl.java
+++ google-apis/src/com/google/gwt/maps/client/impl/IconImpl.java
@@ -28,93 +28,93 @@ import com.google.gwt.maps.client.overlay.Icon;
  */
 public interface IconImpl extends JSFlyweightWrapper {
 
-  public static IconImpl impl = (IconImpl) GWT.create(IconImpl.class);
+  IconImpl impl = (IconImpl) GWT.create(IconImpl.class);
 
   /**
    * @gwt.binding
    */
-  public void bind(JavaScriptObject jsoPeer, Icon icon);
+  void bind(JavaScriptObject jsoPeer, Icon icon);
 
   /**
    * @gwt.constructor $wnd.GIcon
    */
-  public JavaScriptObject construct();
+  JavaScriptObject construct();
 
   /**
    * @gwt.constructor $wnd.GIcon
    */
-  public JavaScriptObject construct(Icon icon);
+  JavaScriptObject construct(Icon icon);
 
   /**
    * @gwt.constructor $wnd.GIcon
    */
-  public JavaScriptObject construct(Icon icon, String url);
+  JavaScriptObject construct(Icon icon, String url);
 
   /**
    * @gwt.global $wnd.G_DEFAULT_ICON
    */
-  public JavaScriptObject getDefaultIcon();
+  JavaScriptObject getDefaultIcon();
 
-  public String getDragCrossImage(JavaScriptObject jsoPeer);
+  Point getDragCrossAnchor(JavaScriptObject jsoPeer);
 
-  public Point getDragCrossAnchor(JavaScriptObject jsoPeer);
+  String getDragCrossImage(JavaScriptObject jsoPeer);
 
-  public Size getDragCrossSize(JavaScriptObject jsoPeer);
+  Size getDragCrossSize(JavaScriptObject jsoPeer);
 
-  public Point getIconAnchor(JavaScriptObject jsoPeer);
+  Point getIconAnchor(JavaScriptObject jsoPeer);
 
-  public Size getIconSize(JavaScriptObject jsoPeer);
+  Size getIconSize(JavaScriptObject jsoPeer);
 
-  public String getImage(JavaScriptObject jsoPeer);
+  String getImage(JavaScriptObject jsoPeer);
 
   /**
    * @gwt.typeArgs <java.lang.Integer>
    */
-  public JSList getImageMap(JavaScriptObject jsoPeer);
+  JSList getImageMap(JavaScriptObject jsoPeer);
 
-  public Point getInfoWindowAnchor(JavaScriptObject jsoPeer);
+  Point getInfoWindowAnchor(JavaScriptObject jsoPeer);
 
-  public int getMaxHeight(JavaScriptObject jsoPeer);
+  int getMaxHeight(JavaScriptObject jsoPeer);
 
-  public String getMozPrintImage(JavaScriptObject jsoPeer);
+  String getMozPrintImage(JavaScriptObject jsoPeer);
 
-  public String getPrintImage(JavaScriptObject jsoPeer);
+  String getPrintImage(JavaScriptObject jsoPeer);
 
-  public String getShadow(JavaScriptObject jsoPeer);
+  String getShadow(JavaScriptObject jsoPeer);
 
-  public Size getShadowSize(JavaScriptObject jsoPeer);
+  Size getShadowSize(JavaScriptObject jsoPeer);
 
-  public String getTransparent(JavaScriptObject jsoPeer);
+  String getTransparent(JavaScriptObject jsoPeer);
 
-  public void setDragCrossImage(JavaScriptObject jsoPeer, String url);
+  void setDragCrossAnchor(JavaScriptObject jsoPeer, Point anchor);
 
-  public void setDragCrossAnchor(JavaScriptObject jsoPeer, Point anchor);
+  void setDragCrossImage(JavaScriptObject jsoPeer, String url);
 
-  public void setDragCrossSize(JavaScriptObject jsoPeer, Size size);
+  void setDragCrossSize(JavaScriptObject jsoPeer, Size size);
 
-  public void setIconAnchor(JavaScriptObject jsoPeer, Point anchor);
+  void setIconAnchor(JavaScriptObject jsoPeer, Point anchor);
 
-  public void setIconSize(JavaScriptObject jsoPeer, Size size);
+  void setIconSize(JavaScriptObject jsoPeer, Size size);
 
-  public void setImage(JavaScriptObject jsoPeer, String url);
+  void setImage(JavaScriptObject jsoPeer, String url);
 
   /**
    * @gwt.typeArgs imageMap <java.lang.Integer>
    */
-  public void setImageMap(JavaScriptObject jsoPeer, JSList/* int[] */imageMap);
+  void setImageMap(JavaScriptObject jsoPeer, JSList/* int[] */imageMap);
 
-  public void setInfoWindowAnchor(JavaScriptObject jsoPeer, Point anchor);
+  void setInfoWindowAnchor(JavaScriptObject jsoPeer, Point anchor);
 
-  public void setMaxHeight(JavaScriptObject jsoPeer, int height);
+  void setMaxHeight(JavaScriptObject jsoPeer, int height);
 
   // TODO: why is this different than getPrintImageUrl?
-  public void setMozPrintImage(JavaScriptObject jsoPeer, String url);
+  void setMozPrintImage(JavaScriptObject jsoPeer, String url);
 
-  public void setPrintImage(JavaScriptObject jsoPeer, String url);
+  void setPrintImage(JavaScriptObject jsoPeer, String url);
 
-  public void setShadow(JavaScriptObject jsoPeer, String url);
+  void setShadow(JavaScriptObject jsoPeer, String url);
 
-  public void setShadowSize(JavaScriptObject jsoPeer, Size size);
+  void setShadowSize(JavaScriptObject jsoPeer, Size size);
 
-  public void setTransparent(JavaScriptObject jsoPeer, String url);
+  void setTransparent(JavaScriptObject jsoPeer, String url);
 }
