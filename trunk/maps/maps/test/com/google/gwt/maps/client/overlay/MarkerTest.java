1aababc2d2a33d9732e91d1b53f3e1e8932b6488
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2008-06-20 14:29:26 +0000

    fixes issue 141
    There is no need for Marker to be final, and it is darn handy 
    to subclass it sometimes.  I added a unit test that demonstrates that.
    
    Found by: monfouquoi
    Patch by: zundel
    Review by: mmendez

--- trunk/maps/maps/test/com/google/gwt/maps/client/overlay/MarkerTest.java
+++ trunk/maps/maps/test/com/google/gwt/maps/client/overlay/MarkerTest.java
@@ -18,6 +18,8 @@ package com.google.gwt.maps.client.overlay;
 import com.google.gwt.junit.client.GWTTestCase;
 import com.google.gwt.maps.client.MapWidget;
 import com.google.gwt.maps.client.TestUtilities;
+import com.google.gwt.maps.client.event.MarkerClickHandler;
+import com.google.gwt.maps.client.event.MarkerClickHandler.MarkerClickEvent;
 import com.google.gwt.maps.client.geom.LatLng;
 import com.google.gwt.maps.client.geom.Point;
 import com.google.gwt.maps.client.geom.Size;
@@ -47,15 +49,15 @@ public class MarkerTest extends GWTTestCase {
    */
   public void testIconAccessors() {
     Icon ic = new Icon();
-    
-   Point pointA = new Point(10, 20);
+
+    Point pointA = new Point(10, 20);
     ic.setDragCrossAnchor(pointA);
     assertEquals("DragCrossAnchor", pointA, ic.getDragCrossAnchor());
-    
+
     String dragCrossImage = "DragCrossImage";
     ic.setDragCrossImageURL(dragCrossImage);
     assertEquals("DragCrossImageURL", dragCrossImage, ic.getDragCrossImageUrl());
-    
+
     Size size1 = new Size(20, 20);
     ic.setDragCrossSize(size1);
     assertEquals("DragCrossSize", size1, ic.getDragCrossSize());
@@ -63,33 +65,33 @@ public class MarkerTest extends GWTTestCase {
     Point pointB = new Point(20, 30);
     ic.setIconAnchor(pointB);
     assertEquals("IconAnchor", pointB, ic.getIconAnchor());
-    
+
     Size size2 = new Size(2, 2);
     ic.setIconSize(size2);
     assertEquals("IconSize", size2, ic.getIconSize());
-    
+
     int[] map1 = {1, 2, 3, 4};
     ic.setImageMap(map1);
     assertEquals("ImageMap length", map1.length, ic.getImageMap().length);
     for (int i = 0; i < map1.length; i++) {
       assertEquals("ImageMap", map1[i], ic.getImageMap()[i]);
     }
-    
+
     String dummyImage = "DummyImage";
     ic.setImageURL(dummyImage);
     assertEquals("ImageURL", dummyImage, ic.getImageURL());
-    
-    Point pointC = new Point(100,20);
+
+    Point pointC = new Point(100, 20);
     ic.setInfoWindowAnchor(pointC);
     assertEquals("InfoWindowAnchor", pointC, ic.getInfoWindowAnchor());
 
     ic.setMaxHeight(10);
     assertEquals("MaxHeight", 10, ic.getMaxHeight());
-    
+
     dummyImage = "DummyMozPrintImage";
     ic.setMozPrintImageURL(dummyImage);
     assertEquals("MozPrintImageURL", dummyImage, ic.getMozPrintImageURL());
-    
+
     dummyImage = "PrintImage";
     ic.setPrintImageURL(dummyImage);
     assertEquals("PrintImageURL", dummyImage, ic.getPrintImageURL());
@@ -97,11 +99,11 @@ public class MarkerTest extends GWTTestCase {
     Size size3 = new Size(12, 34);
     ic.setShadowSize(size3);
     assertEquals("ShadowSize", size3, ic.getShadowSize());
-    
+
     String dummyShadowURL = "DummyShadowURL";
     ic.setShadowURL(dummyShadowURL);
     assertEquals("Shadow URL", dummyShadowURL, ic.getShadowURL());
-    
+
     String transparentUrl = "TransparentURL";
     ic.setTransparentImageURL(transparentUrl);
     assertEquals("TransarentURL", transparentUrl, ic.getTransparentImageURL());
@@ -156,5 +158,42 @@ public class MarkerTest extends GWTTestCase {
     Marker m = new Marker(new LatLng(33.7814790, -84.3880580), mo);
     map.addOverlay(m);
     RootPanel.get().add(map);
+  }
+
+  /**
+   * Test of a marker subclass.
+   */
+  public void testSubclassMarker() {
+    class MyMarker extends Marker {
+      final String infoText;
+
+      MyMarker(LatLng latlng, String text) {
+        super(latlng);
+        infoText = text;
+      }
+    }
+
+    final String marker1Text = "Marker One";
+
+    MyMarker marker1 = new MyMarker(new LatLng(1, 1), marker1Text);
+    marker1.addMarkerClickHandler(new MarkerClickHandler() {
+
+      public void onClick(MarkerClickEvent event) {
+        Marker m = event.getSender();
+        assertTrue("instanceof MyMarker", m instanceof MyMarker);
+        MyMarker myMarker = (MyMarker) m;
+        assertEquals("marker 1 click event", marker1Text, myMarker.infoText);
+        finishTest();
+      }
+
+    });
+
+    final MapWidget map = new MapWidget();
+    map.setSize("300px", "300px");
+
+    map.addOverlay(marker1);
+    delayTestFinish(ASYNC_DELAY_MSEC);
+    
+    marker1.trigger(new MarkerClickEvent(marker1));
   }
 }
