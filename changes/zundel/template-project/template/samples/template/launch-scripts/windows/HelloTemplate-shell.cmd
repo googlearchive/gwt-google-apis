dac94b0f3bb67c2d2c2967ac75c2651166ab0340
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2008-07-03 16:17:01 +0000

    Fixes for the default ant target to build

--- changes/zundel/template-project/template/samples/template/launch-scripts/windows/HelloTemplate-shell.cmd
+++ changes/zundel/template-project/template/samples/template/launch-scripts/windows/HelloTemplate-shell.cmd
@@ -1,5 +1,5 @@
 @if "%GWT_HOME%"=="" goto needGWTHome
-@java -Xmx128m -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-template.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\..\www" %* com.google.gwt.template.sample.template.HelloTemplate/HelloTemplate.html
+@java -Xmx128m -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-template.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\..\www" %* com.google.template.gwt.sample.template.HelloTemplate/HelloTemplate.html
 @exit /B %ERRORLEVEL%
 
 :needGWTHome
