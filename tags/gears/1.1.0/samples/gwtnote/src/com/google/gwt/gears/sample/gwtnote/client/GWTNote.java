4201ad4cf49251f09f2d6262b2b3c37a60b9739a
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2008-08-26 17:38:59 +0000

    Tagging the Gears 1.1RC1 (1.1.0) release.

---
+++ tags/gears/1.1.0/samples/gwtnote/src/com/google/gwt/gears/sample/gwtnote/client/GWTNote.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2007 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.gears.sample.gwtnote.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.gears.sample.gwtnote.client.ui.RichTextWidget;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * A GWT application demonstrating how to use Gears in GWT.
+ * @see AppController
+ */
+public class GWTNote implements EntryPoint {
+  private RichTextWidget mainPanel = null;
+
+  private AppController controller = null;
+
+  /**
+   * GWT application entry point.
+   */
+  public void onModuleLoad() {
+    mainPanel = new RichTextWidget(this);
+    RootPanel.get().add(mainPanel);
+
+    // the AppController does the heavy lifting
+    controller = new AppController();
+    controller.setWidget(mainPanel);
+    controller.startMainLoop();
+  }  
+}
