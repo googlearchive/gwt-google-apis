4ba17033bc3691e1063e25756b14297b268e64f7
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2008-10-15 10:25:02 +0000

    Adds support for message types other than string.
    Patch by: zundel
    Review by: ajr

--- releases/gears/1.1/gears/test/com/google/gwt/gears/client/workerpool/WorkerPoolTest.java
+++ releases/gears/1.1/gears/test/com/google/gwt/gears/client/workerpool/WorkerPoolTest.java
@@ -15,17 +15,94 @@
  */
 package com.google.gwt.gears.client.workerpool;
 
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.core.client.JsArrayBoolean;
+import com.google.gwt.core.client.JsArrayInteger;
+import com.google.gwt.core.client.JsArrayNumber;
+import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.gears.client.Factory;
 import com.google.gwt.junit.client.GWTTestCase;
 
 /**
- * 
+ * Test the Gears WorkerPool API.
  */
 public class WorkerPoolTest extends GWTTestCase {
-  private static final String WORKER_JS_SRC = ""
+  /**
+   * Used internally to swap messages between worker and main thread.
+   */
+  private static class FooMessage extends JavaScriptObject {
+    protected FooMessage() {
+    }
+
+    public final native int getFoo() /*-{
+      return this.foo;
+    }-*/;
+
+    public final native void setFoo(int value) /*-{
+      this.foo = value;
+    }-*/;
+  }
+
+  private static final String WORKER_JS_SRC = "" + "function workerInit() {"
+      + "  google.gears.workerPool.onmessage = function(a, b, message){"
+      + "     google.gears.workerPool.sendMessage('done', message.sender);"
+      + "  };" + "}" + "workerInit();";
+
+  private static final String WORKER_JS_SRC_BOOL = ""
       + "function workerInit() {"
-      + "  google.gears.workerPool.onmessage = function(messageString, srcWorkerId){"
-      + "     google.gears.workerPool.sendMessage('done', srcWorkerId);"
+      + "  google.gears.workerPool.onmessage = function(a, b, message){"
+      + "     var returnMessage = 'error';"
+      + "     if (message.body === true) { returnMessage = 'done'; }"
+      + "     google.gears.workerPool.sendMessage(returnMessage, message.sender);"
+      + "  };" + "}" + "workerInit();";
+
+  private static final String WORKER_JS_SRC_DOUBLE = ""
+      + "function workerInit() {"
+      + "  google.gears.workerPool.onmessage = function(a, b, message){"
+      + "     var returnMessage = 'error';"
+      + "     if (message.body == 1.0) { returnMessage = 'done'; }"
+      + "     google.gears.workerPool.sendMessage(returnMessage, message.sender);"
+      + "  };" + "}" + "workerInit();";
+
+  private static final String WORKER_JS_SRC_MAKE_TYPE = ""
+      + "function workerInit() {"
+      + "  google.gears.workerPool.onmessage = function(a, b, message) {"
+      + "     var returnMessage = 'error';"
+      + "     if (message.body == 'boolean') { "
+      + "       returnMessage = true; "
+      + "     } else if (message.body == 'Boolean') { "
+      + "       returnMessage = new Boolean(true); "
+      + "     } else if (message.body == 'string') { "
+      + "       returnMessage = 'stringValue'; "
+      + "     } else if (message.body == 'String') { "
+      + "       returnMessage = new String('stringValue'); "
+      + "     } else if (message.body == 'number') { "
+      + "       returnMessage = 1.0; "
+      + "     } else if (message.body == 'Number') { "
+      + "       returnMessage = new Number(1.0); "
+      + "     } else if (message.body == 'object') { "
+      + "       returnMessage = new Object(); returnMessage.foo = 1; "
+      + "     } else if (message.body == 'arrayBoolean') { "
+      + "       returnMessage = [ true, false, true, false, true ];"
+      + "     } else if (message.body == 'arrayInteger') { "
+      + "       returnMessage = [ 1, 2, 3 ];"
+      + "     } else if (message.body == 'arrayNumber') { "
+      + "       returnMessage = [ 1.1, 2.2, 3.3, 4.4 ];"
+      + "     } else if (message.body == 'arrayObject') { "
+      + "       returnMessage = [ {foo:1}, {foo:2} ];"
+      + "     } else if (message.body == 'arrayString') { "
+      + "       returnMessage = [ 'foo', 'bar' ];"
+      + "     }"
+      + "     google.gears.workerPool.sendMessage(returnMessage, message.sender);"
+      + "  };" + "}" + "workerInit();";
+
+  private static final String WORKER_JS_SRC_OBJ = ""
+      + "function workerInit() {"
+      + "  google.gears.workerPool.onmessage = function(a, b, message){"
+      + "     var returnMessage = 'error';"
+      + "     if (message.body.foo == 1) { returnMessage = 'done'; }"
+      + "     google.gears.workerPool.sendMessage(returnMessage, message.sender);"
       + "  };" + "}" + "workerInit();";
 
   /**
@@ -48,6 +125,42 @@ public class WorkerPoolTest extends GWTTestCase {
     }
   }
 
+  public void disabledTestReceiveMessageNumberBoxed() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageString()", event.isMessageString());
+        assertTrue("isMessageObject()", event.isMessageObject());
+        assertFalse("!isMessageBoolean()", event.isMessageBoolean());
+        assertFalse("isMessageNumber()", event.isMessageNumber());
+        assertEquals(1.0, event.getBodyNumber());
+        finishTest();
+      }
+    });
+    wp.sendMessage("Number", workerID);
+  }
+
+  public void disabledTestReceiveMessageStringBoxed() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageNumber()", event.isMessageNumber());
+        assertTrue("isMessageObject()", event.isMessageObject());
+        assertFalse("!isMessageBoolean", event.isMessageBoolean());
+        assertFalse("isMessageString()", event.isMessageString());
+        assertEquals("stringValue", event.getBody());
+        finishTest();
+      }
+    });
+    wp.sendMessage("String", workerID);
+  }
+
   /**
    * Test method for {@link WorkerPool#sendMessage(String, int)}.
    */
@@ -64,7 +177,7 @@ public class WorkerPoolTest extends GWTTestCase {
     WorkerPool wp = Factory.getInstance().createWorkerPool();
     int workerID = wp.createWorker(WORKER_JS_SRC);
 
-    wp.sendMessage(null, workerID);
+    wp.sendMessage((String) null, workerID);
     wp.sendMessage("", -2);
   }
 
@@ -97,7 +210,7 @@ public class WorkerPoolTest extends GWTTestCase {
     WorkerPool wp = Factory.getInstance().createWorkerPool();
     wp.setMessageHandler(new WorkerPoolMessageHandler() {
       public void onMessageReceived(MessageEvent event) {
-        // purposely empty
+        // intentionally empty
       }
     });
 
@@ -106,10 +219,275 @@ public class WorkerPoolTest extends GWTTestCase {
     wp.createWorker("");
   }
 
+  public void testReceiveMessageArrayBoolean() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageNumber()", event.isMessageNumber());
+        assertFalse("!isMessageString()", event.isMessageString());
+        assertTrue("isMessageObject()", event.isMessageObject());
+        assertFalse("!isMessageBoolean()", event.isMessageBoolean());
+        JsArrayBoolean result = event.getBodyArrayBoolean();
+        assertEquals("length of array", 5, result.length());
+        assertEquals("array[0]", true, result.get(0));
+        assertEquals("array[1]", false, result.get(1));
+        assertEquals("array[2]", true, result.get(2));
+        assertEquals("array[3]", false, result.get(3));
+        assertEquals("array[4]", true, result.get(4));
+        finishTest();
+      }
+    });
+    wp.sendMessage("arrayBoolean", workerID);
+  }
+
+  public void testReceiveMessageArrayInteger() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageNumber()", event.isMessageNumber());
+        assertFalse("!isMessageString()", event.isMessageString());
+        assertTrue("isMessageObject()", event.isMessageObject());
+        assertFalse("!isMessageBoolean()", event.isMessageBoolean());
+        JsArrayInteger result = event.getBodyArrayInteger();
+        assertEquals("length of array", 3, result.length());
+        assertEquals("array[0]", 1, result.get(0));
+        assertEquals("array[1]", 2, result.get(1));
+        assertEquals("array[2]", 3, result.get(2));
+        finishTest();
+      }
+    });
+    wp.sendMessage("arrayInteger", workerID);
+  }
+
+  public void testReceiveMessageArrayNumber() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageNumber()", event.isMessageNumber());
+        assertFalse("!isMessageString()", event.isMessageString());
+        assertTrue("isMessageObject()", event.isMessageObject());
+        assertFalse("!isMessageBoolean()", event.isMessageBoolean());
+        JsArrayNumber result = event.getBodyArrayNumber();
+        assertEquals("length of array", 4, result.length());
+        assertEquals("array[0]", 1.1, result.get(0));
+        assertEquals("array[1]", 2.2, result.get(1));
+        assertEquals("array[2]", 3.3, result.get(2));
+        assertEquals("array[3]", 4.4, result.get(3));
+        finishTest();
+      }
+    });
+    wp.sendMessage("arrayNumber", workerID);
+  }
+
+  public void testReceiveMessageArrayObject() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      @SuppressWarnings("unchecked")
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageNumber()", event.isMessageNumber());
+        assertFalse("!isMessageString()", event.isMessageString());
+        assertTrue("isMessageObject()", event.isMessageObject());
+        assertFalse("!isMessageBoolean()", event.isMessageBoolean());
+        JsArray<FooMessage> result = (JsArray<FooMessage>) event.getBodyArray();
+        assertEquals("length of array", 2, result.length());
+        FooMessage m = result.get(0);
+        assertEquals("array[0]", 1, m.getFoo());
+        m = result.get(1);
+        assertEquals("array[1]", 2, m.getFoo());
+        finishTest();
+      }
+    });
+    wp.sendMessage("arrayObject", workerID);
+  }
+
+  public void testReceiveMessageArrayString() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageNumber()", event.isMessageNumber());
+        assertFalse("!isMessageString()", event.isMessageString());
+        assertTrue("isMessageObject()", event.isMessageObject());
+        assertFalse("!isMessageBoolean()", event.isMessageBoolean());
+        JsArrayString result = event.getBodyArrayString();
+        assertEquals("length of array", 2, result.length());
+        assertEquals("array[0]", "foo", result.get(0));
+        assertEquals("array[1]", "bar", result.get(1));
+        finishTest();
+      }
+    });
+    wp.sendMessage("arrayString", workerID);
+  }
+
+  public void testReceiveMessageBoolean() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageNumber()", event.isMessageNumber());
+        assertFalse("!isMessageString()", event.isMessageString());
+        assertFalse("!isMessageObject()", event.isMessageObject());
+        assertTrue("isMessageBoolean()", event.isMessageBoolean());
+        assertEquals(true, event.getBodyBoolean());
+        finishTest();
+      }
+    });
+    wp.sendMessage("boolean", workerID);
+  }
+
+  public void testReceiveMessageBooleanBoxed() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageNumber()", event.isMessageNumber());
+        assertFalse("!isMessageString()", event.isMessageString());
+        assertTrue("isMessageObject()", event.isMessageObject());
+        assertFalse("isMessageBoolean()", event.isMessageBoolean());
+        assertEquals(true, event.getBodyBoolean());
+        finishTest();
+      }
+    });
+    wp.sendMessage("Boolean", workerID);
+  }
+
+  public void testReceiveMessageNumber() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageString()", event.isMessageString());
+        assertFalse("!isMessageObject()", event.isMessageObject());
+        assertFalse("!isMessageBoolean()", event.isMessageBoolean());
+        assertTrue("isMessageNumber()", event.isMessageNumber());
+        assertEquals(1.0, event.getBodyNumber());
+        finishTest();
+      }
+    });
+    wp.sendMessage("number", workerID);
+  }
+
+  public void testReceiveMessageObject() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageNumber()", event.isMessageNumber());
+        assertFalse("!isMessageString()", event.isMessageString());
+        assertFalse("!isMessageBoolean()", event.isMessageBoolean());
+        assertTrue("isMessageObject()", event.isMessageObject());
+        FooMessage fooObj = event.getBodyObject().cast();
+        assertEquals(1, fooObj.getFoo());
+        finishTest();
+      }
+    });
+    wp.sendMessage("object", workerID);
+  }
+
+  public void testReceiveMessageString() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_MAKE_TYPE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        // Window.alert("Got message as string: " + event.getBody());
+        assertFalse("!isMessageNumber()", event.isMessageNumber());
+        assertFalse("!isMessageObject()", event.isMessageObject());
+        assertFalse("!isMessageBoolean", event.isMessageBoolean());
+        assertTrue("isMessageString()", event.isMessageString());
+        assertEquals("stringValue", event.getBody());
+        finishTest();
+      }
+    });
+    wp.sendMessage("string", workerID);
+  }
+
+  /**
+   * Test method for {@link WorkerPool#sendMessage(boolean, int)}.
+   */
+  public void testSendMessageBoolean() {
+
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_BOOL);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        assertEquals("done", event.getBody());
+        finishTest();
+      }
+    });
+    wp.sendMessage(true, workerID);
+  }
+
   /**
    * Test method for {@link WorkerPool#sendMessage(String, int)}.
    */
-  public void testSendMessage() {
+  public void testSendMessageEmpty() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC);
+    wp.sendMessage("", workerID);
+  }
+
+  /**
+   * Test method for {@link WorkerPool#sendMessage(double, int)}.
+   */
+  public void testSendMessageNumber() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_DOUBLE);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        assertEquals("done", event.getBody());
+        finishTest();
+      }
+    });
+    wp.sendMessage(1.0, workerID);
+  }
+
+  /**
+   * Test method for {@link WorkerPool#sendMessage(JavaScriptObject, int)}.
+   */
+  public void testSendMessageObject() {
+    WorkerPool wp = Factory.getInstance().createWorkerPool();
+    int workerID = wp.createWorker(WORKER_JS_SRC_OBJ);
+    delayTestFinish(5000);
+    wp.setMessageHandler(new WorkerPoolMessageHandler() {
+      public void onMessageReceived(MessageEvent event) {
+        assertEquals("done", event.getBody());
+        finishTest();
+      }
+    });
+    FooMessage foo = FooMessage.createObject().cast();
+    foo.setFoo(1);
+    wp.sendMessage(foo, workerID);
+  }
+
+  /**
+   * Test method for {@link WorkerPool#sendMessage(String, int)}.
+   */
+  public void testSendMessageString() {
     WorkerPool wp = Factory.getInstance().createWorkerPool();
     int workerID = wp.createWorker(WORKER_JS_SRC);
     delayTestFinish(5000);
@@ -120,14 +498,5 @@ public class WorkerPoolTest extends GWTTestCase {
       }
     });
     wp.sendMessage("foo", workerID);
-  }
-
-  /**
-   * Test method for {@link WorkerPool#sendMessage(String, int)}.
-   */
-  public void testSendMessageEmpty() {
-    WorkerPool wp = Factory.getInstance().createWorkerPool();
-    int workerID = wp.createWorker(WORKER_JS_SRC);
-    wp.sendMessage("", workerID);
   }
 }
