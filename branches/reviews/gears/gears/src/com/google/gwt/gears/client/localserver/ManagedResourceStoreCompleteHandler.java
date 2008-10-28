b14160be68a7331d66afa2ef41a7fef52bc39023
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2008-10-28 21:57:41 +0000

    For code reviews of gears

---
+++ branches/reviews/gears/gears/src/com/google/gwt/gears/client/localserver/ManagedResourceStoreCompleteHandler.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2008 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.gears.client.localserver;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+/**
+ * Provides an interface to implement in order to receive update complete
+ * events.
+ */
+public interface ManagedResourceStoreCompleteHandler {
+  /**
+   * Encapsulates the arguments to the update complete event.
+   */
+  final class ManagedResourceStoreCompleteEvent extends JavaScriptObject {
+    protected ManagedResourceStoreCompleteEvent() {
+      // Required for overlay types
+    }
+
+    /**
+     * Returns the new version of the {@link ManagedResourceStore} if the
+     * version changed, empty otherwise.
+     * 
+     * @return the new version of the {@link ManagedResourceStore} if the
+     *         version changed, empty otherwise
+     */
+    public native String getNewVersion() /*-{
+      return this.newVersion;
+    }-*/;
+  }
+
+  /**
+   * Method to be invoked when a {@link ManagedResourceStore} fires it update
+   * complete event.
+   * 
+   * @param event contains the properties of the event
+   */
+  void onComplete(ManagedResourceStoreCompleteEvent event);
+}
---
+++ branches/reviews/gears/samples/managedresourcestore/launch-scripts/windows/ManagedResourceStoreDemo-shell.cmd
@@ -0,0 +1,8 @@
+@if "%GWT_HOME%"=="" goto needGWTHome
+@java -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-gears.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\..\www" %* com.google.gwt.gears.sample.managedresourcestore.ManagedResourceStoreDemo/ManagedResourceStoreDemo.html
+@exit /B %ERRORLEVEL%
+
+:needGWTHome
+@echo The environment variable GWT_HOME is not defined, it should point to a valid GWT installation.
+@exit /B 1
+
