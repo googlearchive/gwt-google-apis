bb4a8fd6bd53299e5467d566ed2b270b27a9c24b
zundel <zundel@google.com>
2008-11-19 19:15:31 +0000

    Tagging release from http://gwt-google-apis.googlecode.com/maps/1.0 as release 1.0.1 at r947

---
+++ 1.0.1/maps/src/com/google/gwt/maps/client/event/PolylineRemoveHandler.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2008 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.maps.client.event;
+
+import com.google.gwt.maps.client.overlay.Polyline;
+
+import java.util.EventObject;
+
+/**
+ * Provides an interface to implement in order to receive MapEvent.REMOVE events
+ * from the {@link Polyline}.
+ */
+public interface PolylineRemoveHandler {
+
+  /**
+   * Encapsulates the arguments for the MapEvent.REMOVE event on a
+   * {@link Polyline}.
+   */
+  @SuppressWarnings("serial")
+  class PolylineRemoveEvent extends EventObject {
+
+    public PolylineRemoveEvent(Polyline source) {
+      super(source);
+    }
+
+    /**
+     * Returns the instance of the polyline that generated this event.
+     * 
+     * @return the instance of the polyline that generated this event.
+     */
+    public Polyline getSender() {
+      return (Polyline) getSource();
+    }
+  }
+
+  /**
+   * Method to be invoked when a MapEvent.REMOVE event fires on a
+   * {@link Polyline}.
+   * 
+   * @param event contains the properties of the event.
+   */
+  void onRemove(PolylineRemoveEvent event);
+}
