6ee9948601ef28e048bb6bef9477e1550c74383e
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2008-12-08 17:01:34 +0000

    Renamed the package com.google.gwt.visualization.sample. to com.google.gwt.visualization. This was done to shorten some path names for Windows, which complained when decompressing the .zip distribution with the built-in compressed folder support.
    Patch by: zundel
    Review by: uwe.maurer

--- releases/visualization/1.0/samples/customvisualization/launch-scripts/windows/CustomVisualization-compile.cmd
+++ releases/visualization/1.0/samples/customvisualization/launch-scripts/windows/CustomVisualization-compile.cmd
@@ -1,5 +1,5 @@
 @if "%GWT_HOME%"=="" goto needGWTHome
-@java -Xmx128M -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-visualization.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\..\..\www" %* com.google.gwt.visualization.sample.customvisualization.CustomVisualization
+@java -Xmx128M -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-visualization.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\..\..\www" %* com.google.gwt.visualization.customvisualization.CustomVisualization
 @exit /B %ERRORLEVEL%
 
 :needGWTHome
