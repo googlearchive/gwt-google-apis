18fd550e06d74ecea2018aeee6b5804d3808d96c
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2009-02-06 22:28:47 +0000

    Tagging visualization 1.0.1 at r1255

---
+++ tags/visualization/1.0.1/samples/hellovisualization/launch-scripts/windows/HelloVisualization-shell.cmd
@@ -0,0 +1,7 @@
+@if "%GWT_HOME%"=="" goto needGWTHome
+@java -Xmx256m -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-visualization.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\..\www" %* com.google.gwt.visualization.hellovisualization.HelloVisualization/HelloVisualization.html
+@exit /B %ERRORLEVEL%
+
+:needGWTHome
+@echo The environment variable GWT_HOME is not defined, it should point to a valid GWT installation.
+@exit /B 1
---
+++ tags/visualization/1.0.1/visualization/test/com/google/gwt/visualization/client/VisualizationTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2008 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.visualization.client;
+
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.junit.client.GWTTestCase;
+import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
+import com.google.gwt.visualization.client.visualizations.Visualization;
+
+/**
+ * A base class to support testing the visualization API that requires loading.
+ */
+public class VisualizationTest extends GWTTestCase {
+  public static final int ASYNC_DELAY_MS = 10 * 1000;
+  
+  /**
+   * Extracts the value of a named parameter from a URL query string.
+   * 
+   * @param url the URL to extract the parameter from
+   * @param name the name of the parameter
+   * @return the value of the parameter
+   */
+  public static native String getParameter(String url, String name) /*-{
+    var spec = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(spec);
+    var results = regex.exec(url);
+    if(results == null) {
+      return "";
+    } else {
+      return results[1];
+    }
+  }-*/;
+
+  private boolean loaded = false;
+
+  public VisualizationTest() {
+    super();
+  }
+
+  public AbstractDataTable createCompanyPerformance() {
+    DataTable data = DataTable.create();
+    data.addColumn(ColumnType.STRING, "Year");
+    data.addColumn(ColumnType.NUMBER, "Sales");
+    data.addColumn(ColumnType.NUMBER, "Expenses");
+    data.addRows(4);
+    data.setValue(0, 0, "2004");
+    data.setValue(0, 1, 1000);
+    data.setValue(0, 2, 400);
+    data.setValue(1, 0, "2005");
+    data.setValue(1, 1, 1170);
+    data.setValue(1, 2, 460);
+    data.setValue(2, 0, "2006");
+    data.setValue(2, 1, 660);
+    data.setValue(2, 2, 1120);
+    data.setValue(3, 0, "2007");
+    data.setValue(3, 1, 1030);
+    data.setValue(3, 2, 540);
+    return data;
+  }
+
+  @Override
+  public String getModuleName() {
+    return "com.google.gwt.visualization.VisualizationTest";
+  }
+
+  public void test() {
+  }
+
+  /**
+   * A support method used by several test cases to create a simple DataTable.
+   */
+  protected DataTable createDailyActivities() {
+    // Create a simple data table
+    DataTable data = DataTable.create();
+    data.addColumn(ColumnType.STRING, "Fruit");
+    data.addColumn(ColumnType.NUMBER, "Baskets");
+    data.addRows(3);
+    data.setValue(0, 0, "Apple");
+    data.setValue(0, 1, 11);
+    data.setValue(1, 0, "Banana");
+    data.setValue(1, 1, 3);
+    data.setValue(2, 0, "Cherry");
+    data.setValue(2, 1, 7);
+
+    return data;
+  }
+
+  protected String getVisualizationPackage() {
+    throw new UnsupportedOperationException();
+  }
+
+  protected void loadApi(final Runnable testRunnable) {
+    if (loaded) {
+      testRunnable.run();
+    } else {
+      AjaxLoader.loadVisualizationApi(new Runnable() {
+        public void run() {
+          loaded = true;
+          testRunnable.run();
+          finishTest();
+        }
+      }, getVisualizationPackage());
+      delayTestFinish(ASYNC_DELAY_MS);
+    }
+  }
+
+  /**
+   * See <a href=
+   * "http://code.google.com/apis/visualization/documentation/dev/events.html" >
+   * Google Visualization API documentation</a> .
+   * 
+   * @param viz - the Visualization to trigger the event on
+   * @param s - a selection object.
+   */
+  protected <E extends Visualization<?>, Selectable>void triggerSelection(E viz, JsArray<Selection> s) {
+    Selection.triggerSelection(viz, s);
+  }
+}
