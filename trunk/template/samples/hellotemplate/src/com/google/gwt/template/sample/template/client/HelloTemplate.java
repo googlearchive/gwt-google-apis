5710d1e28870fe1e53bdd46d5043889b1ddb93c2
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2009-02-08 23:27:28 +0000

    Updated after making AjaxLoader project from template

--- trunk/template/samples/hellotemplate/src/com/google/gwt/template/sample/template/client/HelloTemplate.java
+++ trunk/template/samples/hellotemplate/src/com/google/gwt/template/sample/template/client/HelloTemplate.java
@@ -1,4 +1,4 @@
-package com.google.gwt.template.sample.hellotemplate.client;
+package com.google.gwt.template.sample.template.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;
