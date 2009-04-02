5a8345916677658ba2e59c21474e67927040fb31
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2009-04-02 12:29:12 +0000

    Integrates AjaxLoader support into the project by adding an svn:externals property to the ajaxloader project.  Also adds a second .jar file that excludes the ajaxloader in case you want a project without external dependencies.
    Reviewed by: goutham.enigma

--- changes/goutham/language-orig/samples/hellolanguage/src/com/google/gwt/language/sample/hellolanguage/client/HelloLanguage.java
+++ changes/goutham/language-orig/samples/hellolanguage/src/com/google/gwt/language/sample/hellolanguage/client/HelloLanguage.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,6 +16,7 @@
 package com.google.gwt.language.sample.hellolanguage.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.language.client.LanguageUtils;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
@@ -24,10 +25,27 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class HelloLanguage implements EntryPoint {
 
   /**
-   * This is the entry point method.
+   * The onModuleLoad() method is called when the body of the document is
+   * finished loading. The JavaScript APIs are not loaded unless they were
+   * included in the body of the .html file. Use the LanguageUtils.loadXXX()
+   * methods to load them after the app starts, but before any API calls are
+   * made.
    */
   public void onModuleLoad() {
-    TranslationDemo translationDemo = new TranslationDemo();
-    RootPanel.get().add(translationDemo.getDemoPanel());
+    // TODO: Use a tabbed interface for the demo? 
+    LanguageUtils.loadTranslation(new Runnable() {
+
+      public void run() {
+        TranslationDemo translationDemo = new TranslationDemo();
+        RootPanel.get("translateDemo").add(translationDemo.getDemoPanel());
+      }
+    });
+    LanguageUtils.loadTransliteration(new Runnable() {
+
+      public void run() {
+        TransliterationDemo transliterateDemo = new TransliterationDemo();
+        RootPanel.get("transliterateDemo").add(transliterateDemo.getDemoPanel());
+      }
+    });
   }
 }
--- changes/goutham/language-orig/samples/hellolanguage/src/com/google/gwt/language/sample/hellolanguage/client/TransliterationDemo.java
+++ changes/goutham/language-orig/samples/hellolanguage/src/com/google/gwt/language/sample/hellolanguage/client/TransliterationDemo.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,9 +15,22 @@
  */
 package com.google.gwt.language.sample.hellolanguage.client;
 
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
 /**
- * Transliteration demo
+ * Transliteration demo.
  */
 public class TransliterationDemo {
+  private final VerticalPanel mainPanel = new VerticalPanel();
 
+  /**
+   * Returns the panel associated with this demo.
+   * 
+   * @return the main panel
+   */
+  public Panel getDemoPanel() {
+    return mainPanel;
+  }
+  
 }
