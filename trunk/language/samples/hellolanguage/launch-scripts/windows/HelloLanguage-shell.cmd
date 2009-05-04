c67c04e93ed70ad2595fe24a7c981a9e270e251f
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2009-05-04 15:49:42 +0000

    Fixed launch scripts for windows.

--- trunk/language/samples/hellolanguage/launch-scripts/windows/HelloLanguage-shell.cmd
+++ trunk/language/samples/hellolanguage/launch-scripts/windows/HelloLanguage-shell.cmd
@@ -1,5 +1,5 @@
 @if "%GWT_HOME%"=="" goto needGWTHome
-@java -Xmx128m -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-language.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\..\www" %* com.google.gwt.language.sample.language.HelloLanguage/HelloLanguage.html
+@java -Xmx128m -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-language.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\..\www" %* com.google.gwt.language.sample.hellolanguage.HelloLanguage/HelloLanguage.html
 @exit /B %ERRORLEVEL%
 
 :needGWTHome
