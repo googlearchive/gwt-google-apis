0268568a91503ab11971e1cf354b596efd0362f6
goutham.enigma <goutham.enigma@dc8c9c99-1131-0410-92e2-872299cb2494>
2009-05-05 07:04:36 +0000

    Updating the comments in LanguageUtils.java

--- language/language/src/com/google/gwt/language/client/LanguageUtils.java
+++ language/language/src/com/google/gwt/language/client/LanguageUtils.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,7 +20,6 @@ import com.google.gwt.ajaxloader.client.AjaxLoader.AjaxLoaderOptions;
 
 /**
  * Utility class for the Google Language API.
- * 
  */
 public class LanguageUtils {
 
@@ -28,7 +27,7 @@ public class LanguageUtils {
   public static final String TRANSLITERATION_API_NAME = "elements";
 
   /**
-   * Call this method to load the Language API before using any API methods.
+   * Call this method to load the Translation API before using any API methods.
    * This will load the jsapi script from www.google.com and invoke the
    * google.load() method.
    */
@@ -37,6 +36,11 @@ public class LanguageUtils {
     AjaxLoader.loadApi(TRANSLATION_API_NAME, "1", onLoad, null);
   }
 
+  /**
+   * Call this method to load Transliteration API before using the API methods.
+   * To specify extra options for loading transliteration API, you can
+   * use {@code AjaxLoader.loadApi()} directly.
+   */
   public static void loadTransliteration(Runnable onLoad) {
     assert (onLoad != null);
     AjaxLoaderOptions options = AjaxLoaderOptions.newInstance();
