b7d90a546773feec063bb6b38d22d03ed2a34871
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2009-06-29 13:14:39 +0000

    Post 1.0 Merge to correct the sample for problems with new release of transliteration:
    svn merge -r1427:1515 https://gwt-google-apis.googlecode.com/svn/trunk/language .

--- releases/language/1.0/language/test/com/google/gwt/language/LanguageTestSuite.java
+++ releases/language/1.0/language/test/com/google/gwt/language/LanguageTestSuite.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,6 +16,7 @@
 package com.google.gwt.language;
 
 import com.google.gwt.junit.tools.GWTTestSuite;
+import com.google.gwt.language.client.translation.LanguageEnumTest;
 import com.google.gwt.language.client.translation.TranslationTest;
 import com.google.gwt.language.client.transliteration.control.ListenerManagerTest;
 import com.google.gwt.language.client.transliteration.control.TransliterationControlTest;
@@ -32,6 +33,7 @@ public class LanguageTestSuite extends GWTTestSuite {
   public static Test suite() {
     TestSuite suite = new TestSuite("Tests for Language API");
     suite.addTestSuite(TranslationTest.class);
+    suite.addTestSuite(LanguageEnumTest.class);
     suite.addTestSuite(TransliterationControlTest.class);
     suite.addTestSuite(ListenerManagerTest.class);
     suite.addTestSuite(TransliterationTest.class);
