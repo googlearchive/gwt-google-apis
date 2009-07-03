866a478ac12af582bdff61bc6ad89c60d08009bb
zundel <zundel@google.com>
2009-07-03 22:41:22 +0000

    Fixes issue 288 The setOnProgressHandler() method had a typo and set the wrong property for the callback.
    Patch by: anonymous.coward6 (tototo)
    Review by: zundel

--- gears/gears/src/com/google/gwt/gears/client/localserver/ManagedResourceStore.java
+++ gears/gears/src/com/google/gwt/gears/client/localserver/ManagedResourceStore.java
@@ -279,7 +279,7 @@ public final class ManagedResourceStore extends JavaScriptObject {
    */
   public native void setOnProgressHandler(
       ManagedResourceStoreProgressHandler handler) /*-{
-    this.progress = function(details) {
+    this.onprogress = function(details) {
       @com.google.gwt.gears.client.localserver.ManagedResourceStore::fireOnProgress(Lcom/google/gwt/gears/client/localserver/ManagedResourceStoreProgressHandler;Lcom/google/gwt/gears/client/localserver/ManagedResourceStoreProgressHandler$ManagedResourceStoreProgressEvent;)(handler, details);
     };
   }-*/;
