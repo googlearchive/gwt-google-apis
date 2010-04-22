4893563be078e5c5c4b1660430700c7dcf8cd0b2
piotr.swigon <piotr.swigon@dc8c9c99-1131-0410-92e2-872299cb2494>
2010-04-22 20:10:31 +0000

    Issue 391801: Removes platform-dependent launch scripts review by: Zundel

--- ajaxloader/samples/helloajaxloader/launch-scripts/windows/HelloAjaxLoader-compile.cmd
+++
@@ -1,7 +0,0 @@
-@if "%GWT_HOME%"=="" goto needGWTHome
-@java -Xmx256M -cp "%~dp0\..\..\src;%~dp0\..\..\war\WEB-INF\classes;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;%GWT_HOME%\gwt-dev.jar;..\..\..\..\gwt-ajaxloader.jar" com.google.gwt.dev.Compiler -war "%~dp0\..\..\war" %* com.google.gwt.ajaxloader.sample.helloajaxloader.HelloAjaxLoader
-@exit /B %ERRORLEVEL%
-
-:needGWTHome
-@echo The environment variable GWT_HOME is not defined, it should point to a valid GWT installation.
-@exit /B 1
