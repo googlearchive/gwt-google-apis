b8e16b694ba33119a107ec850276357be1cf805b
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2010-04-28 20:52:01 +0000

    This patch finishes up the conversion to use AjaxLoader in the maps test cases
    Review by: piotrs@google.com
    http://gwt-code-reviews.appspot.com/423801/show

--- trunk/maps/maps/test/com/google/gwt/maps/client/overlay/TrafficOverlayTest.java
+++ trunk/maps/maps/test/com/google/gwt/maps/client/overlay/TrafficOverlayTest.java
@@ -15,18 +15,15 @@
  */
 package com.google.gwt.maps.client.overlay;
 
-import com.google.gwt.junit.client.GWTTestCase;
 import com.google.gwt.maps.client.MapWidget;
+import com.google.gwt.maps.client.MapsTestCase;
 import com.google.gwt.maps.client.TestUtilities;
 import com.google.gwt.maps.client.event.TrafficOverlayChangedHandler;
 import com.google.gwt.maps.client.event.TrafficOverlayChangedHandler.TrafficOverlayChangedEvent;
 import com.google.gwt.maps.client.geom.LatLng;
 import com.google.gwt.user.client.ui.RootPanel;
 
-public class TrafficOverlayTest extends GWTTestCase {
-  // length of time to wait for asynchronous test to complete.
-  static final int ASYNC_DELAY_MSEC = 5000;
-
+public class TrafficOverlayTest extends MapsTestCase {
   @Override
   public String getModuleName() {
     return "com.google.gwt.maps.GoogleMapsTest";
@@ -44,53 +41,66 @@ public class TrafficOverlayTest extends GWTTestCase {
    * Test the simple TrafficOverlay constructor.
    */
   public void testTrafficOverlay() {
-    MapWidget map = new MapWidget(LatLng.newInstance(0, 0), 3);
-    map.setSize("500px", "400px");
-    TrafficOverlay traffic = new TrafficOverlay();
-    map.addOverlay(traffic);
-    RootPanel.get().add(map);
+    loadApi(new Runnable() {
+      public void run() {
+        MapWidget map = new MapWidget(LatLng.newInstance(0, 0), 3);
+
+        map.setSize("500px", "400px");
+        TrafficOverlay traffic = new TrafficOverlay();
+        map.addOverlay(traffic);
+        RootPanel.get().add(map);
+      }
+    });
   }
 
   /**
    * Test the TrafficOverlay constructor with options.
    */
   public void testTrafficOverlayOptions() {
-    MapWidget map = new MapWidget(LatLng.newInstance(0, 0), 3);
-    map.setSize("500px", "400px");
-    TrafficOverlay traffic = new TrafficOverlay(
-        TrafficOverlayOptions.newInstance(true));
-    map.addOverlay(traffic);
-    RootPanel.get().add(map);
+    loadApi(new Runnable() {
+      public void run() {
+        MapWidget map = new MapWidget(LatLng.newInstance(0, 0), 3);
+        map.setSize("500px", "400px");
+        TrafficOverlay traffic = new TrafficOverlay(
+            TrafficOverlayOptions.newInstance(true));
+        map.addOverlay(traffic);
+        RootPanel.get().add(map);
+      }
+    });
   }
 
   /**
    * Test the TrafficOverlay constructor with options.
    */
   public void testTrafficOverlayOptions2() {
-    MapWidget map = new MapWidget(LatLng.newInstance(0, 0), 3);
-    map.setSize("500px", "400px");
-    TrafficOverlay traffic = new TrafficOverlay(
-        TrafficOverlayOptions.newInstance(false));
-    map.addOverlay(traffic);
-    RootPanel.get().add(map);
+    loadApi(new Runnable() {
+      public void run() {
+        MapWidget map = new MapWidget(LatLng.newInstance(0, 0), 3);
+        map.setSize("500px", "400px");
+        TrafficOverlay traffic = new TrafficOverlay(
+            TrafficOverlayOptions.newInstance(false));
+        map.addOverlay(traffic);
+        RootPanel.get().add(map);
+      }
+    });
   }
 
   /**
    * Test the "changed" event using a trigger.
    */
   public void testTrafficOverlayChangedTrigger() {
-    final MapWidget m = new MapWidget();
-    final TrafficOverlay traffic = new TrafficOverlay();
-
-    m.addOverlay(traffic);
-    traffic.addTrafficOverlayChangedHandler(new TrafficOverlayChangedHandler() {
-
-      public void onChanged(TrafficOverlayChangedEvent event) {
-        finishTest();
+    loadApi(new Runnable() {
+      public void run() {
+        final MapWidget m = new MapWidget();
+        final TrafficOverlay traffic = new TrafficOverlay();
+        m.addOverlay(traffic);
+        traffic.addTrafficOverlayChangedHandler(new TrafficOverlayChangedHandler() {
+          public void onChanged(TrafficOverlayChangedEvent event) {
+            finishTest();
+          }
+        });
+        traffic.trigger(new TrafficOverlayChangedEvent(traffic, true));
       }
-
-    });
-    delayTestFinish(ASYNC_DELAY_MSEC);
-    traffic.trigger(new TrafficOverlayChangedEvent(traffic, true));
+    }, false);
   }
 }
