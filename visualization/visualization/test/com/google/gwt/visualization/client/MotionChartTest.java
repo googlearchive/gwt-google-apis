d5cb72411be23b671a3428115746562c57511934
piotr.swigon <piotr.swigon@dc8c9c99-1131-0410-92e2-872299cb2494>
2010-04-30 18:58:17 +0000

    Issue 367804: Fixes MotionChartTest review by:Zundel

--- visualization/visualization/test/com/google/gwt/visualization/client/MotionChartTest.java
+++ visualization/visualization/test/com/google/gwt/visualization/client/MotionChartTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -28,11 +28,16 @@ import java.util.Date;
 
 /**
  * Tests for the MotionChart class.
- * 
+ *
  * Note that only one instance of MotionChart is allowed at a time, which kind
  * of restricts this unit test.
  */
 public class MotionChartTest extends VisualizationTest {
+
+  @Override
+  protected void gwtSetUp() throws Exception {
+    cleanDom();
+  }
 
   /**
    * This test case will try creating a simple MotionChart. It first asserts
@@ -156,7 +161,7 @@ public class MotionChartTest extends VisualizationTest {
   }
 
   private native void triggerStateChanged(JavaScriptObject jso) /*-{
-    $wnd.google.visualization.events.trigger(jso, 'statechanged', 
+    $wnd.google.visualization.events.trigger(jso, 'statechanged',
     {});
   }-*/;
 }
