e99f8f579f55bfe888c0d92130a8ecaa40b584d8
piotr.swigon <piotr.swigon@dc8c9c99-1131-0410-92e2-872299cb2494>
2010-05-14 14:55:00 +0000

    Issue 526801: Removes use of deperecated code from Maps API review by: Zundel

--- trunk/maps/maps/src/com/google/gwt/maps/jsio/rebind/JSWrapperGenerator.java
+++ trunk/maps/maps/src/com/google/gwt/maps/jsio/rebind/JSWrapperGenerator.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -88,7 +88,7 @@ public class JSWrapperGenerator extends Generator {
    * Annotation's value method, a {@link Proxy} will be synthesized. The proxy
    * mode is only to support existing functionality, all new features should be
    * added via new annotations.
-   * 
+   *
    * @param <A> the desired type of Annotation
    * @param <M> the type of object to search
    * @param logger a logger
@@ -256,6 +256,7 @@ public class JSWrapperGenerator extends Generator {
   /**
    * Entry point into the Generator.
    */
+  @Override
   public final String generate(TreeLogger logger, GeneratorContext context,
       java.lang.String typeName) throws UnableToCompleteException {
 
