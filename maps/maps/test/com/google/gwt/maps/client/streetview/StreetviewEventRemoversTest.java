44a6041ddc5c3c16598f58a30f52fd75b778ebea
zundel <zundel@google.com>
2010-06-17 16:16:56 +0000

    Tries to make maps unit tests more stable on the continuous build system by splitting up the streetview widget tests into multiple modules.  This will cause a module load between runs.
    Review by: piotrs
    http://gwt-code-reviews.appspot.com/628801/show

--- maps/maps/test/com/google/gwt/maps/client/streetview/StreetviewEventRemoversTest.java
+++ maps/maps/test/com/google/gwt/maps/client/streetview/StreetviewEventRemoversTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -27,14 +27,13 @@ import com.google.gwt.maps.client.event.StreetviewPitchChangedHandler.Streetview
 import com.google.gwt.maps.client.event.StreetviewYawChangedHandler.StreetviewYawChangedEvent;
 import com.google.gwt.maps.client.event.StreetviewZoomChangedHandler.StreetviewZoomChangedEvent;
 import com.google.gwt.maps.client.geom.LatLng;
-import com.google.gwt.maps.client.streetview.StreetviewPanoramaWidget.ErrorValue;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Tests for {@link StreetviewPanoramaWidget} and supporting classes.
  */
-public class StreetviewPanoramaWidgetTest extends MapsTestCase {
+public class StreetviewEventRemoversTest extends MapsTestCase {
 
   /**
    * HACK: Workaround to avoid running these tests on FF. Tests including Flash
@@ -47,16 +46,19 @@ public class StreetviewPanoramaWidgetTest extends MapsTestCase {
 
   private StreetviewPanoramaWidget panorama;
 
+ 
+
   /**
    * Using different module to force browser refresh for
    * {@link StreetviewPanoramaWidget} tests.
    */
   @Override
   public String getModuleName() {
-    return "com.google.gwt.maps.GoogleStreetviewTest";
+    return "com.google.gwt.maps.GoogleStreetviewEventRemoversTest";
   }
 
-  public void testAddErrorHandler() {
+  
+  public void testRemoveErrorHandler() {
     /**
      * HACK: Workaround to avoid running these tests on FF. Tests including
      * Flash plugin fail on FF if screen of the tester machine is locked.
@@ -65,110 +67,45 @@ public class StreetviewPanoramaWidgetTest extends MapsTestCase {
       return;
     }
     loadApi(new Runnable() {
-      public void run() {
-        final LatLng mountEverest = LatLng.newInstance(27.988056, 86.925278);
-        StreetviewPanoramaOptions options = StreetviewPanoramaOptions.newInstance();
-        options.setLatLng(mountEverest);
+      private int handleCount = 0;
+      private boolean initialized = false;
 
-        panorama = new StreetviewPanoramaWidget(options);
-        panorama.setSize("500px", "300px");
+      public void run() {
+        panorama = newDefaultPanorama();
 
         panorama.addErrorHandler(new StreetviewErrorHandler() {
           public void onError(StreetviewErrorEvent event) {
-            assertEquals(ErrorValue.NO_NEARBY_PANO.getErrorCode(),
-                event.getErrorCode());
-            finishTest();
-          }
-        });
-
-        RootPanel.get().add(panorama);
-      }
-    }, false);
-  }
-
-  public void testAddInitializedHandler() {
-    /**
-     * HACK: Workaround to avoid running these tests on FF. Tests including
-     * Flash plugin fail on FF if screen of the tester machine is locked.
-     */
-    if (isFirefoxOnWindows()) {
-      return;
-    }
-    loadApi(new Runnable() {
-      public void run() {
-        final LatLng tenthStreet = LatLng.newInstance(33.7814839, -84.3879353);
-        final Pov pov = Pov.newInstance();
-        pov.setPitch(-5).setYaw(180).setZoom(1);
-
-        StreetviewPanoramaOptions options = StreetviewPanoramaOptions.newInstance();
-        options.setLatLng(tenthStreet);
-        options.setPov(pov);
-
-        panorama = new StreetviewPanoramaWidget(options);
-        panorama.setSize("500px", "300px");
-
-        panorama.addInitializedHandler(new StreetviewInitializedHandler() {
-          public void onInitialized(StreetviewInitializedEvent event) {
-            LatLng point = event.getLocation().getLatLng();
-            assertEquals(tenthStreet.getLatitude(), point.getLatitude(), 1e-2);
-            assertEquals(tenthStreet.getLongitude(), point.getLongitude(), 1e-2);
-
-            Pov actualPov = event.getLocation().getPov();
-            assertEquals(pov.getPitch(), actualPov.getPitch(), 1e-2);
-            assertEquals(pov.getYaw(), actualPov.getYaw(), 1e-2);
-            // Intentionally not testing zoom, as it behaves unpredictably
-
-            assertEquals(panorama, event.getSender());
-            assertNotNull(event.getLocation().getPanoId());
-            assertNotNull(event.getLocation().getDescription());
-            finishTest();
-          }
-        });
-
-        RootPanel.get().add(panorama);
-      }
-    }, false);
-  }
-
-  public void testAddInitializedHandler_changedLocation() {
-    /**
-     * HACK: Workaround to avoid running these tests on FF. Tests including
-     * Flash plugin fail on FF if screen of the tester machine is locked.
-     */
-    if (isFirefoxOnWindows()) {
-      return;
-    }
-    loadApi(new Runnable() {
-      public void run() {
-        final LatLng tenthStreet = LatLng.newInstance(33.7814839, -84.3879353);
-        final LatLng manhattan = LatLng.newInstance(40.728333, -73.994167);
-        panorama = newDefaultPanorama();
-
-        panorama.addInitializedHandler(new StreetviewInitializedHandler() {
-          public void onInitialized(StreetviewInitializedEvent event) {
-            LatLng point = event.getLocation().getLatLng();
-
-            // If on tenth street in Atlanta
-            if (Math.abs(tenthStreet.getLatitude() - point.getLatitude()) < 1e-2
-                && Math.abs(tenthStreet.getLongitude() - point.getLongitude()) < 1e-2) {
-              // Event triggered when panorama was initialized first time
-              panorama.setLocationAndPov(manhattan, Pov.newInstance());
-            } else {
-              // Event triggered when panorama was initialized again after
-              // setLocationAndPov() was invoked
-              assertEquals(manhattan.getLatitude(), point.getLatitude(), 1e-2);
-              assertEquals(manhattan.getLongitude(), point.getLongitude(), 1e-2);
-              finishTest();
+            if (handleCount++ != 0) {
+              fail("Handler used more then once");
             }
+
+            panorama.removeErrorHandler(this);
+          }
+        });
+
+        panorama.addInitializedHandler(new StreetviewInitializedHandler() {
+          public void onInitialized(StreetviewInitializedEvent event) {
+            initialized = true;
+            panorama.trigger(new StreetviewErrorEvent(panorama, 0));
+            panorama.trigger(new StreetviewErrorEvent(panorama, 0));
           }
         });
 
         RootPanel.get().add(panorama);
+
+        new Timer() {
+          @Override
+          public void run() {
+            assertEquals("Expected one and only one error callback.  initialized="
+                + initialized, 1, handleCount);
+            finishTest();
+          }
+        }.schedule(6 * 1000);
       }
     }, false);
   }
 
-  public void testAddPitchChangedHandler() {
+  public void testRemoveInitializedHandler() {
     /**
      * HACK: Workaround to avoid running these tests on FF. Tests including
      * Flash plugin fail on FF if screen of the tester machine is locked.
@@ -177,35 +114,86 @@ public class StreetviewPanoramaWidgetTest extends MapsTestCase {
       return;
     }
     loadApi(new Runnable() {
+      private int handleCount = 0;
+      private boolean initialized = false;
+      
       public void run() {
         panorama = newDefaultPanorama();
 
-        final double pitch = 30;
+        panorama.addInitializedHandler(new StreetviewInitializedHandler() {
+          public void onInitialized(StreetviewInitializedEvent event) {
+            initialized = true;
+            if (handleCount++ != 0) {
+              fail("Handler used more then once");
+            }
+
+            panorama.removeInitializedHandler(this);
+            panorama.trigger(new StreetviewInitializedEvent(panorama, null));
+          }
+        });
+
+        RootPanel.get().add(panorama);
+
+        new Timer() {
+          @Override
+          public void run() {
+            assertEquals("Expected one and only one initialized callback.  initialized="
+                + initialized, 1, handleCount);
+            finishTest();
+          }
+        }.schedule(5 * 1000);
+      }
+    }, false);
+  }
+
+  public void testRemovePitchChangedHandler() {
+    /**
+     * HACK: Workaround to avoid running these tests on FF. Tests including
+     * Flash plugin fail on FF if screen of the tester machine is locked.
+     */
+    if (isFirefoxOnWindows()) {
+      return;
+    }
+    loadApi(new Runnable() {
+      private int handleCount = 0;
+      private boolean initialized = false;
+      
+      public void run() {
+        panorama = newDefaultPanorama();
 
         panorama.addPitchChangedHandler(new StreetviewPitchChangedHandler() {
           public void onPitchChanged(StreetviewPitchChangedEvent event) {
-            assertEquals(panorama, event.getSender());
-            // 'If' instead of 'assert', as this event might be triggered few
-            // times before we invoke setPov() method
-            if (Math.abs(pitch - event.getPitch()) < 1e-2) {
-              finishTest();
+            if (handleCount++ != 0) {
+              fail("Handler used more then once");
             }
+
+            panorama.removePitchChangedHandler(this);
           }
         });
 
         panorama.addInitializedHandler(new StreetviewInitializedHandler() {
           public void onInitialized(StreetviewInitializedEvent event) {
-            Pov pov = Pov.newInstance().setPitch(pitch);
-            panorama.setPov(pov);
+            initialized = true;
+            panorama.trigger(new StreetviewPitchChangedEvent(panorama, 0));
+            panorama.trigger(new StreetviewPitchChangedEvent(panorama, 0));
           }
         });
 
         RootPanel.get().add(panorama);
+
+        new Timer() {
+          @Override
+          public void run() {
+            assertEquals("Expected one and only one pitch changed callback.  initialized="
+                + initialized, 1, handleCount);
+            finishTest();
+          }
+        }.schedule(6 * 1000);
       }
     }, false);
   }
 
-  public void testAddYawChangedHandler() {
+  public void testRemoveYawChangedHandler() {
     /**
      * HACK: Workaround to avoid running these tests on FF. Tests including
      * Flash plugin fail on FF if screen of the tester machine is locked.
@@ -214,35 +202,45 @@ public class StreetviewPanoramaWidgetTest extends MapsTestCase {
       return;
     }
     loadApi(new Runnable() {
+      private int handleCount = 0;
+      private boolean initialized = false;
+      
       public void run() {
         panorama = newDefaultPanorama();
-
-        final double yaw = 180;
 
         panorama.addYawChangedHandler(new StreetviewYawChangedHandler() {
           public void onYawChanged(StreetviewYawChangedEvent event) {
-            assertEquals(panorama, event.getSender());
-            // 'If' instead of 'assert', as this event might be triggered few
-            // times before we invoke setPov() method
-            if (Math.abs(yaw - event.getYaw()) < 1e-2) {
-              finishTest();
+            if (handleCount++ != 0) {
+              fail("Handler used more then once");
             }
+
+            panorama.removeYawChangedHandler(this);
           }
         });
 
         panorama.addInitializedHandler(new StreetviewInitializedHandler() {
           public void onInitialized(StreetviewInitializedEvent event) {
-            Pov pov = Pov.newInstance().setYaw(yaw);
-            panorama.setPov(pov);
+            initialized = true;
+            panorama.trigger(new StreetviewYawChangedEvent(panorama, 0));
+            panorama.trigger(new StreetviewYawChangedEvent(panorama, 0));
           }
         });
 
         RootPanel.get().add(panorama);
+
+        new Timer() {
+          @Override
+          public void run() {
+            assertEquals("Expected one and only one yaw changed callback.  initialized="
+                + initialized, 1, handleCount);
+            finishTest();
+          }
+        }.schedule(6 * 1000);
       }
     }, false);
   }
 
-  public void testAddZoomChangedHandler() {
+  public void testRemoveZoomChangedHandler() {
     /**
      * HACK: Workaround to avoid running these tests on FF. Tests including
      * Flash plugin fail on FF if screen of the tester machine is locked.
@@ -251,30 +249,40 @@ public class StreetviewPanoramaWidgetTest extends MapsTestCase {
       return;
     }
     loadApi(new Runnable() {
+      private int handleCount = 0;
+      private boolean initialized = false;
+      
       public void run() {
         panorama = newDefaultPanorama();
 
-        final double zoom = 1;
-
         panorama.addZoomChangedHandler(new StreetviewZoomChangedHandler() {
-          public void onZoomChanged(StreetviewZoomChangedEvent event) {
-            assertEquals(panorama, event.getSender());
-            // 'If' instead of 'assert', as this event might be triggered few
-            // times before we invoke setPov() method
-            if (Math.abs(zoom - event.getZoom()) < 1e-2) {
-              finishTest();
+          public void onZoomChanged(StreetviewZoomChangedEvent event) {            
+            if (handleCount++ != 0) {
+              fail("Handler used more then once");
             }
+
+            panorama.removeZoomChangedHandler(this);
           }
         });
 
         panorama.addInitializedHandler(new StreetviewInitializedHandler() {
           public void onInitialized(StreetviewInitializedEvent event) {
-            Pov pov = Pov.newInstance().setZoom(zoom);
-            panorama.setPov(pov);
+            initialized = true;            
+            panorama.trigger(new StreetviewZoomChangedEvent(panorama, 0));
+            panorama.trigger(new StreetviewZoomChangedEvent(panorama, 0));
           }
         });
 
         RootPanel.get().add(panorama);
+
+        new Timer() {
+          @Override
+          public void run() {
+            assertEquals("Expected one and only one zoom changed callback.  initialized="
+                + initialized, 1, handleCount);
+            finishTest();
+          }
+        }.schedule(6 * 1000);
       }
     }, false);
   }
