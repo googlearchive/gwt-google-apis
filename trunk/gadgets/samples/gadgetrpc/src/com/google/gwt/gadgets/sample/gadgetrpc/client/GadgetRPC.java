f506e37220819f405d5b3bdea872a3cb699b11b3
zundel@google.com <zundel@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2010-06-23 21:36:24 +0000

    Moves the gadgets.rpc package to gadgets.gwtrpc to avoid confusing with gadgets.rpc JS api Review by: piotrs@google.com http://gwt-code-reviews.appspot.com/615802/show

--- trunk/gadgets/samples/gadgetrpc/src/com/google/gwt/gadgets/sample/gadgetrpc/client/GadgetRPC.java
+++ trunk/gadgets/samples/gadgetrpc/src/com/google/gwt/gadgets/sample/gadgetrpc/client/GadgetRPC.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -23,7 +23,7 @@ import com.google.gwt.gadgets.client.UserPreferences;
 import com.google.gwt.gadgets.client.Gadget.AllowHtmlQuirksMode;
 import com.google.gwt.gadgets.client.Gadget.ModulePrefs;
 import com.google.gwt.gadgets.client.Gadget.UseLongManifestName;
-import com.google.gwt.gadgets.client.rpc.GadgetsRpc;
+import com.google.gwt.gadgets.client.gwtrpc.GadgetsGwtRpc;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
@@ -70,7 +70,7 @@ public class GadgetRPC extends Gadget<GadgetRPC.Preferences> {
     String rpcUrl = serviceDef.getServiceEntryPoint();
 
     // Uses Gadgets container as proxy for GWT RPC requests
-    GadgetsRpc.redirectThroughProxy(serviceDef);
+    GadgetsGwtRpc.redirectThroughProxy(serviceDef);
 
     // Build the user interface.
     VerticalPanel vp = new VerticalPanel();
