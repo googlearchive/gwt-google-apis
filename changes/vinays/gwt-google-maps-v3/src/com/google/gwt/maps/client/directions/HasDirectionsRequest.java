98d54ae43bcb0e281485136a4e2d6d90b431990f
vinays@google.com <vinays@google.com@dc8c9c99-1131-0410-92e2-872299cb2494>
2010-11-08 16:05:46 +0000

    Changed copyright owner from Vinay Inc. to Google Inc.

--- changes/vinays/gwt-google-maps-v3/src/com/google/gwt/maps/client/directions/HasDirectionsRequest.java
+++ changes/vinays/gwt-google-maps-v3/src/com/google/gwt/maps/client/directions/HasDirectionsRequest.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010 Vinay Inc.
+/* Copyright (c) 2010 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* Copyright (c) 2010 Vinay Inc.
+/* Copyright (c) 2010 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
