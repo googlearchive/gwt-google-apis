facc35e9e09567ec0eef8321840d72e063d2ccaa
jasonhall <jasonhall@google.com>
2011-04-25 20:03:41 +0000

    Persist auth tokens in cookies, and check the state of them when authenticating: * If the token is found and will not expire soon, immediately return the token. * If the token is found and may expire soon, authenticate via an iframe instead of showing the auto-closing popup. **EDIT** This has been removed from this CL; for now, the popup will always be used. * If the token is not found, assume access has not been granted and display the popup, which may auto-close.
    The token is stored in cookies, as a mapping from
    clientId + scopes -> token + expires
    
    Since so much more is done in the Auth class, actually write tests for
    it. They're JUnit tests, but I could write GWT tests for the
    window-opening part too. Maybe later.
    
    Update the Explorer to use the new Auth API -- this is pretty
    straightforward.
    
    Update the GWT client support code to use the new Auth API. Note that
    this somewhat breaks the API of the GWT client, since it no longer
    requires a CodeProvider implementation. Another option would be to
    change WindowCodeProvider to call Auth.login() but that would require a
    change to the CodeProvider interface to pass in the request parts
    separately. I'm open to this but it seemed like a bigger change.
    
    R=robertvawter
    DELTA=1058  (821 added, 139 deleted, 98 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=1549

--- src/com/google/api/gwt/client/impl/ClientOAuth2Login.java
+++ src/com/google/api/gwt/client/impl/ClientOAuth2Login.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,6 +15,9 @@
  */
 package com.google.api.gwt.client.impl;
 
+import com.google.api.gwt.oauth2.client.Auth;
+import com.google.api.gwt.oauth2.client.AuthRequest;
+import com.google.api.gwt.oauth2.client.LoginCallback;
 import com.google.api.gwt.shared.OAuth2Login;
 import com.google.gwt.core.client.GWT;
 
@@ -22,31 +25,23 @@ import com.google.gwt.core.client.GWT;
  * Intended to be constructed via {@link OAuth2Login.Builder}.
  */
 public class ClientOAuth2Login extends OAuth2Login {
+
   @Override
   protected void configure(CodeProvider codeProvider, final OAuth2LoginReceiver receiver) {
-    if (codeProvider == null) {
-      codeProvider = new WindowCodeProvider();
-    }
-    if (accessToken == null) {
-      String url = authUrl + "?" + //
-          CLIENT_ID + clientId + "&" + //
-          REDIRECT_URI + getRedirectUri() + "&" + //
-          SCOPE + scope + "&" + //
-          RESPONSE_TYPE + getResponseType();
-      codeProvider.getCode(url, new CodeReceiver() {
+    AuthRequest req = new AuthRequest(authUrl, CLIENT_ID).withScopes(scope);
 
-        @Override
-        public void onSuccess(String code) {
-          accessToken = code;
-          receiver.onSuccess(ClientOAuth2Login.this);
-        }
+    Auth.get().login(req, new LoginCallback() {
+      @Override
+      public void onLogin(String token) {
+        accessToken = token;
+        receiver.onSuccess(ClientOAuth2Login.this);
+      }
 
-        @Override
-        public void onFailure(Throwable t) {
-          receiver.onFailure(t);
-        }
-      });
-    }
+      @Override
+      public void onFailure(Throwable caught) {
+        receiver.onFailure(caught);
+      }
+    });
   }
 
   @Override
