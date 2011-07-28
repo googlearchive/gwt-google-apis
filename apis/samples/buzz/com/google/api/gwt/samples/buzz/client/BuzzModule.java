e487edab000bd3e04c87add6247a8ae3d795fc22
jasonhall <jasonhall@google.com>
2011-07-28 21:21:02 +0000

    Fix broken buzz gwt sample
    DELTA=9  (0 added, 0 deleted, 9 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2798

--- apis/samples/buzz/com/google/api/gwt/samples/buzz/client/BuzzModule.java
+++ apis/samples/buzz/com/google/api/gwt/samples/buzz/client/BuzzModule.java
@@ -19,7 +19,7 @@ package com.google.api.gwt.samples.buzz.client;
 import com.google.api.gwt.client.impl.ClientGoogleApiRequestTransport;
 import com.google.api.gwt.client.impl.ClientOAuth2Login;
 import com.google.api.gwt.samples.buzz.shared.BuzzBase;
-import com.google.api.gwt.services.buzz.v1.shared.Buzz;
+import com.google.api.gwt.services.buzz.shared.Buzz;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
