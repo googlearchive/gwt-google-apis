3f37f4e1bfa39bb319e51bbdcad1863e860135e6
zundel <zundel@google.com>
2011-11-01 16:13:33 +0000

    Missing files from r2003

---
+++ gears/samples/gwtnote/src/com/google/gwt/gears/sample/gwtnote/client/ui/RichTextToolbarStrings.java
@@ -0,0 +1,82 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+
+package com.google.gwt.gears.sample.gwtnote.client.ui;
+
+import com.google.gwt.i18n.client.Constants;
+
+/**
+ * This {@link Constants} interface is used to make the toolbar's strings
+ * internationalizable.
+ */
+public interface RichTextToolbarStrings extends Constants {
+
+  String black();
+
+  String blue();
+
+  String bold();
+
+  String color();
+
+  String createLink();
+
+  String font();
+
+  String green();
+
+  String hr();
+
+  String indent();
+
+  String insertImage();
+
+  String italic();
+
+  String justifyCenter();
+
+  String justifyLeft();
+
+  String justifyRight();
+
+  String large();
+
+  String medium();
+
+  String normal();
+
+  String ol();
+
+  String outdent();
+
+  String red();
+
+  String removeFormat();
+
+  String removeLink();
+
+  String size();
+
+  String small();
+
+  String strikeThrough();
+
+  String subscript();
+
+  String superscript();
+
+  String ul();
+
+  String underline();
+
+  String white();
+
+  String xlarge();
+
+  String xsmall();
+
+  String xxlarge();
+
+  String xxsmall();
+
+  String yellow();
+}
